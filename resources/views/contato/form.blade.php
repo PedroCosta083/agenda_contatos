92b9c877eaf5518a97db43d784a0c7e4b65ef2e5
Pedro Costa <pedroh.costa083@gmail.com>
2024-05-27 15:32:58 -0300

    estilizando

--- resources/views/contato/form.blade.php
+++ resources/views/contato/form.blade.php
@@ -6,163 +6,163 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ isset($contato) ? 'Editar Contato' : 'Adicionar Contato' }}</title>
-    <style>
-        .container {
-            display: flex;
-            flex-direction: column;
-            align-items: flex-start;
-            gap: 15px;
-        }
-
-        .container-checkbox {
-            display: grid;
-            grid-template-columns: repeat(7, 3fr);
-        }
-
-        .buttons {
-            display: flex;
-            flex-direction: row;
-            gap: 10px;
-        }
-    </style>
+    @vite('resources/css/app.css')
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body>
-    @if (isset($contato))
-        <form action="{{ route('contato.update', [$contato->id]) }}" method="POST">
-            @method('put')
-        @else
-            <form action="{{ route('contato.store') }}" method="POST">
-    @endif
-    @csrf
-    <div class="container">
-        <div>
-            <label for="nome">Nome</label>
-            <input {{ isset($form) ? $form : null }} value="{{ $contato->nome ?? '' }}" name='nome' id='nome'
-                type="text" required>
+<body class="bg-blue-200 flex flex-col justify-center items-center gap-4 ">
+
+    <div>
+        @if (isset($contato))
+            <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full max-w-xl"
+                action="{{ route('contato.update', [$contato->id]) }}" method="POST">
+                @method('put')
+            @else
+                <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full max-w-xl"
+                    action="{{ route('contato.store') }}" method="POST">
+        @endif
+        @csrf
+        <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="nome">Nome</label>
+            <input {{ isset($form) ? $form : '' }}
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                value="{{ $contato->nome ?? '' }}" name='nome' id='nome' type="text" required>
         </div>
 
         @unless (isset($form))
-            <div>
-                <label for="telefoneInput">Número de Telefone:</label>
-                <input {{ isset($form) ? $form : null }} type="number" id="telefoneInput"
-                    placeholder="Digite o número de telefone">
-                <button {{ isset($form) ? $form : null }} type="button" onclick="adicionarTelefone()">Adicionar</button>
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="telefoneInput">Número de Telefone:</label>
+                <div class="flex flex-row items-center">
+                    <input {{ isset($form) ? $form : '' }}
+                        class="shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        type="number" id="telefoneInput" placeholder="Digite o número de telefone">
+                    <button {{ isset($form) ? $form : '' }}
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2" type="button"
+                        onclick="adicionarTelefone()">Adicionar</button>
+                </div>
             </div>
         @endunless
 
-        <table id="tabelaContatos">
+        <table class="w-full mb-4" id="tabelaContatos">
             <thead>
                 <tr>
-                    <th>Número de Telefone</th>
-                    <th>Tipo</th>
-                    <th>Excluir</th>
+                    <th class="px-4 py-2">Número de Telefone</th>
+                    <th class="px-4 py-2">Tipo</th>
+                    <th class="px-4 py-2">Excluir</th>
                 </tr>
             </thead>
             <tbody id="tbodyTelefones">
                 @isset($contato->telefone)
                     @foreach ($contato->telefone as $telefone)
                         <tr>
-                            <td><input {{ isset($form) ? $form : null }} type="number" name="telefone[]"
-                                    value="{{ $telefone->numero }}" required></td>
+                            <td><input {{ isset($form) ? $form : '' }}
+                                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    type="number" name="telefone[]" value="{{ $telefone->numero }}" required></td>
                             <td>
-                                <select {{ isset($form) ? $form : null }} name="tipotelefone[]" required>
-                                    <option value="1" {{ $telefone->tipotelefone->id == 1 ? 'selected' : '' }}>
-                                        Celular</option>
-                                    <option value="2" {{ $telefone->tipotelefone->id == 2 ? 'selected' : '' }}>
-                                        Fixo
+                                <select {{ isset($form) ? $form : '' }}
+                                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    name="tipotelefone[]" required>
+                                    <option value="1" {{ $telefone->tipotelefone->id == 1 ? 'selected' : '' }}>Celular
+                                    </option>
+                                    <option value="2" {{ $telefone->tipotelefone->id == 2 ? 'selected' : '' }}>Fixo
                                     </option>
                                 </select>
                             </td>
-                            <td><button {{ isset($form) ? $form : null }} type="button"
-                                    onclick="excluirTelefone(this)">Excluir</button></td>
+                            <td><button {{ isset($form) ? $form : '' }}
+                                    class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                                    type="button" onclick="excluirTelefone(this)">Excluir</button></td>
                         </tr>
                     @endforeach
                 @endisset
             </tbody>
         </table>
 
-        <div class="container-checkbox">
+        <div class="mb-4">
             @foreach ($categorias as $key => $categoria)
-                <input type="checkbox" name="categoria[]" value="{{ $key }}"
-                    {{ $checked = (isset($contato->categoria) ? $contato->categoria->contains($key) : false) ? 'checked' : '' }}
-                    {{ isset($form) ? 'disabled' : '' }}>{{ $categoria }}
+                <label class="inline-flex items-center">
+                    <input {{ isset($form) ? $form : '' }} type="checkbox" class="form-checkbox h-5 w-5 text-blue-600"
+                        name="categoria[]" value="{{ $key }}"
+                        {{ $checked = (isset($contato->categoria) ? $contato->categoria->contains($key) : false) ? 'checked' : '' }}
+                        {{ isset($form) ? 'disabled' : '' }}>
+                    <span class="ml-2 text-gray-700">{{ $categoria }}</span>
+                </label>
             @endforeach
         </div>
 
-
-
-        <div>
-            <label for="logradouro">Logradouro</label>
-            <input {{ isset($form) ? $form : null }} value="{{ $contato->endereco->logradouro ?? '' }}"
-                name='logradouro' id='logradouro' type="text" required>
+        <div class="mb-4">
+            <label {{ isset($form) ? $form : '' }}class="block text-gray-700 text-sm font-bold mb-2"
+                for="logradouro">Logradouro</label>
+            <input {{ isset($form) ? $form : '' }}
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                value="{{ $contato->endereco->logradouro ?? '' }}" name='logradouro' id='logradouro' type="text"
+                required>
         </div>
-        <div>
-            <label for="ncasa">N° Casa</label>
-            <input {{ isset($form) ? $form : null }} value="{{ $contato->endereco->numero ?? '' }}" name='numero'
-                id ='numero' type="text" required>
+        <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="ncasa">N° Casa</label>
+            <input {{ isset($form) ? $form : '' }}
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                value="{{ $contato->endereco->numero ?? '' }}" name='numero' id ='numero' type="text" required>
         </div>
-        <div>
-            <label for="cidade">Cidade</label>
-            <input {{ isset($form) ? $form : null }} value="{{ $contato->endereco->cidade ?? '' }}" name='cidade'
-                id='cidade' type="text" required>
+        <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="cidade">Cidade</label>
+            <input {{ isset($form) ? $form : '' }}
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                value="{{ $contato->endereco->cidade ?? '' }}" name='cidade' id='cidade' type="text" required>
         </div>
-        <div>
-            <label for="cep">Cep</label>
-            <input {{ isset($form) ? $form : null }} value="{{ $contato->endereco->cep ?? '' }}" name='cep'
-                id='cep' type="text" required>
+        <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="cep">Cep</label>
+            <input {{ isset($form) ? $form : '' }}
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus
+            focus
+            "
+                value="{{ $contato->endereco->cep ?? '' }}" name='cep' id='cep' type="text" required>
         </div>
-        <div class="buttons">
-            <div>
-                <button type="submit" {{ isset($form) ? $form : null }}>Enviar</button>
-            </div>
+        <div class="flex items-center justify-between">
+            <button {{ isset($form) ? $form : '' }}
+                class="bg-blue-500 hover
+            text-white font-bold py-2 px-4 rounded" type="submit"
+                {{ isset($form) ? $form : null }}>Enviar</button>
             @if (isset($form))
-                <div>
-                    <a href="{{ isset($contato) ? route('contato.edit', $contato->id) : null }}">Editar</a>
-                </div>
+                <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    href="{{ isset($contato) ? route('contato.edit', $contato->id) : null }}">Editar</a>
             @endif
-            @if (Route::currentRouteName() == 'contato.edit')
-                <div>
-                    <a href="{{ isset($contato) ? route('contato.show', $contato->id) : null }}"
-                        {{ isset($form) ? $form : null }}>Voltar</a>
-                </div>
-            @endif
+            <a class="bg-blue-500 hover text-white font-bold py-2 px-4 rounded"
+                href="{{ isset($contato) ? route('contato.index') : null }}"
+                {{ isset($form) ? $form : null }}>Voltar</a>
+
         </div>
-
-    </div>
-    </form>
-
-    @isset($contato)
-        <form action="{{ isset($contato) ? route('contato.destroy', [$contato->id]) : null }}" method="POST">
-            @csrf
-            @method('delete')
-            <button {{ isset($form) ? $form : null }} type="submit">Excluir</button>
         </form>
-    @endisset
+        @if (isset($contato))
+            <form action="{{ isset($contato) ? route('contato.destroy', [$contato->id]) : null }}" method="POST">
+                @csrf
+                @method('delete')
+                <button class="bg-red-500 hover text-white font-bold py-2 px-4 rounded" type="submit"
+                    {{ isset($form) ? $form : null }}>Excluir Contato</button>
+            </form>
+        @endif
+    </div>
     <script>
         function adicionarTelefone() {
             var tabela = document.getElementById("tbodyTelefones");
             var rowCount = tabela.rows.length;
-
             if (rowCount >= 2) {
                 alert("Você já adicionou o número máximo de telefones.");
                 return;
             }
             var telefoneInput = document.getElementById("telefoneInput");
             var telefone = telefoneInput.value.trim();
-
             if (telefone != '' && !verificarNumero(telefone)) {
                 var newRow = document.createElement("tr");
                 newRow.innerHTML = `
-                    <td><input type="number" name="telefone[]" value="${telefone}" required></td>
-                    <td>
-                        <select name="tipotelefone[]" required>
-                            <option value="1">Celular</option>
-                            <option value="2">Fixo</option>
-                        </select>
-                    </td>
-                    <td><button type="button" onclick="excluirTelefone(this)">Excluir</button></td>
-                `;
+            <td><input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="number" name="telefone[]" value="${telefone}" required></td>
+            <td>
+                <select class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="tipotelefone[]" required>
+                    <option value="1">Celular</option>
+                    <option value="2">Fixo</option>
+                </select>
+            </td>
+            <td><button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" type="button" onclick="excluirTelefone(this)">Excluir</button></td>
+        `;
                 document.getElementById("tbodyTelefones").appendChild(newRow);
                 telefoneInput.value = "";
             } else {
@@ -189,7 +189,7 @@
         }
     </script>
 
-
+    @vite('resources/js/app.js')
 </body>
 
 </html>
